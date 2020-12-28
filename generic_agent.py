db44da859470c6ee87ca05a5e18f853021c24692
Dave Radke <dtradke@uwaterloo.ca>
2020-12-28 11:32:04 -0800

    dev

--- generic_agent.py
+++ generic_agent.py
@@ -8,24 +8,24 @@ from agency import PPOAgent
 
 import tensorflow as tf
 
-from tf_agents.environments import py_environment
-from tf_agents.environments import tf_environment
-from tf_agents.environments import utils
-from tf_agents.specs import array_spec
-from tf_agents.environments import wrappers
-from tf_agents.trajectories import time_step as ts
-
-from tf_agents.agents.dqn import dqn_agent
-from tf_agents.drivers import dynamic_step_driver
-from tf_agents.environments import suite_gym
-from tf_agents.environments import tf_py_environment
-from tf_agents.eval import metric_utils
-from tf_agents.metrics import tf_metrics
-from tf_agents.networks import q_network
-from tf_agents.policies import random_tf_policy
-from tf_agents.replay_buffers import tf_uniform_replay_buffer
-from tf_agents.trajectories import trajectory
-from tf_agents.utils import common
+# from tf_agents.environments import py_environment
+# from tf_agents.environments import tf_environment
+# from tf_agents.environments import utils
+# from tf_agents.specs import array_spec
+# from tf_agents.environments import wrappers
+# from tf_agents.trajectories import time_step as ts
+#
+# from tf_agents.agents.dqn import dqn_agent
+# from tf_agents.drivers import dynamic_step_driver
+# from tf_agents.environments import suite_gym
+# from tf_agents.environments import tf_py_environment
+# from tf_agents.eval import metric_utils
+# from tf_agents.metrics import tf_metrics
+# from tf_agents.networks import q_network
+# from tf_agents.policies import random_tf_policy
+# from tf_agents.replay_buffers import tf_uniform_replay_buffer
+# from tf_agents.trajectories import trajectory
+# from tf_agents.utils import common
 
 #
 # arm_totals = np.zeros((3,))
